3e55f120c66f502f03a61317bb28889814be7830
CevicheMixto23 <alfarodezapiero@gmail.com>
2024-09-11 23:09:37 -0500

    feat(ui): :zap: add provider, logic to sendMessage
    I added provider to dependencies and logic when I send a message

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +23,8 @@ class MessageFieldBox extends StatelessWidget {
       suffixIcon: IconButton(
           onPressed: () {
             final textValue = textController.value.text;
-            print(textValue);
             textController.clear();
+            onValue(textValue);
           },
           icon: const Icon(Icons.send)),
     );
@@ -36,6 +38,7 @@ class MessageFieldBox extends StatelessWidget {
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         textController.clear();
+        onValue(value);
         focusNode.requestFocus();
       },
     );
